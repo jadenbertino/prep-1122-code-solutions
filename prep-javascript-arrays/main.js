28e5d90011efff15737b63966ddbbdb1427bd54f
Jaden Bertino <jaden@bertinofamily.com>
2022-11-21 03:56:02 +0000

    prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,18 @@
+const languages = [
+  'html',
+  'css',
+  'javascript',
+  'react',
+  'node',
+  'postgres'
+];
+
+console.log(languages);
+const firstElement = languages[0];
+console.log(`the first element of the array is ${firstElement}`);
+const thirdElement = languages[2];
+console.log(`the third element of the array is ${thirdElement}`);
+const length = languages.length;
+console.log(`the length of the array is ${length}`);
+const lastElement = languages[length - 1];
+console.log(`The last element of the array is ${lastElement}`);
